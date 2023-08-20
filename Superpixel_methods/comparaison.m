592cc0e063af305eff6761747595f9d763a275eb
Lisa Weisbecker <101018816+weisbeckerlisa@users.noreply.github.com>
2023-08-20 17:10:37 -0400

    Update comparaison.m

--- Superpixel_methods/comparaison.m
+++ Superpixel_methods/comparaison.m
@@ -7,7 +7,6 @@ clear; close all; clc;
 % mex -O CFLAGS="\$CFLAGS -Wall -Wextra -W -std=c99" ./SCALP/SCALP_mex.cpp -outdir ./SCALP  
 % mex -O CFLAGS="\$CFLAGS -Wall -Wextra -W -std=c99" ./SH/SuperpixelHierarchyMex.cpp -outdir ./SH  
 
-% Ajouter des dossiers au chemin
 addpath(genpath('.'));
 
 % Dossiers de données
@@ -17,7 +16,6 @@ gt_folders = {'groundtruth/test'}%, 'groundtruth/train', 'groundtruth/val'};
 edge_folder = 'edges';
 ssn_result_folder = 'data/ssn_result';
 
-% Initialiser les métriques pour chaque méthode
 ASA_metrics_slic = [];
 ASA_metrics_lsc = [];
 ASA_metrics_scalp = [];
@@ -38,7 +36,6 @@ GR_metrics_ssn = [];
 
 np_range = [1000, 800, 600, 500, 425, 350, 275, 200, 150, 100, 50, 25, 10];
 
-% Parcourir chaque image et calculer les métriques
 for sp_nbr = np_range
     for folder_idx = 1:length(image_folders)
         image_folder = fullfile(root_folder, image_folders{folder_idx});
@@ -46,7 +43,6 @@ for sp_nbr = np_range
         
         image_files = dir(fullfile(image_folder, '*.jpg'));
        
-        % Initialize per folder metric storage
         ASA_folder_metrics_slic = [];
         ASA_folder_metrics_lsc = [];
         ASA_folder_metrics_scalp = [];
@@ -94,7 +90,6 @@ for sp_nbr = np_range
             % Lire résultat pré-calculé de SSN
             S_SSN = load(fullfile(ssn_result_folder, [name, '_', num2str(sp_nbr), '_label.mat'])).label;
 
-            % Reordering
             S_slic = sp_reorder_fct(S_slic);
             S_lsc = sp_reorder_fct(S_lsc);
             S_scalp = sp_reorder_fct(S_scalp);
@@ -118,8 +113,7 @@ for sp_nbr = np_range
                 ASA_image_metrics_sh = [ASA_image_metrics_sh, asa_metric(S_SH, gt)];
                 ASA_image_metrics_ssn = [ASA_image_metrics_ssn, asa_metric(S_SSN, gt)];
             end
-
-            % Append average metrics of this image to the folder metrics
+            
             ASA_folder_metrics_slic = [ASA_folder_metrics_slic, mean(ASA_image_metrics_slic)];
             ASA_folder_metrics_lsc = [ASA_folder_metrics_lsc, mean(ASA_image_metrics_lsc)];
             ASA_folder_metrics_scalp = [ASA_folder_metrics_scalp, mean(ASA_image_metrics_scalp)];
@@ -139,7 +133,6 @@ for sp_nbr = np_range
             GR_folder_metrics_ssn = [GR_folder_metrics_ssn, gr_metric(S_SSN)];
         end
         
-        % Append average metrics of this folder to overall metrics
         ASA_metrics_slic = [ASA_metrics_slic, mean(ASA_folder_metrics_slic)];
         ASA_metrics_lsc = [ASA_metrics_lsc, mean(ASA_folder_metrics_lsc)];
         ASA_metrics_scalp = [ASA_metrics_scalp, mean(ASA_folder_metrics_scalp)];
